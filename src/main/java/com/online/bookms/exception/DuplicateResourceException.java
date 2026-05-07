cf9b5033bb65ac7ed07e6021ed7ca25f88cb35a4
IT25103674 <it25103674@my.sliit.lk>
2026-05-07 19:35:44 +0530

    Updated file architecture

--- src/main/java/com/online/bookms/exception/DuplicateResourceException.java
+++
@@ -1,8 +0,0 @@
-package com.online.bookms.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
