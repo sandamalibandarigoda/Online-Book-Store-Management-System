5fd0c6bcda1c30b71ccc9cb7c97ef688b109cbbc
it25103674 <it25103674@my.sliit.lk>
2026-05-07 19:35:44 +0530

    Updated file architecture

--- src/main/java/com/online/bookms/dto/category/CategoryResponse.java
+++
@@ -1,9 +0,0 @@
-package com.online.bookms.dto.category;
-
-public record CategoryResponse(
-        Long id,
-        String name,
-        String description,
-        Long parentCategoryId
-) {
-}
